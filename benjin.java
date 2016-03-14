ea0a63749b525840791503559af4e8574776d8bf
wdh960408 <568311374@qq.com>
2016-03-14 13:02:43 +0800

    benjin

---
+++ benjin.java
@@ -0,0 +1,17 @@
+package fuli;
+
+public class benjin {
+	 public static void main(String[] args){ 
+	 int years=30;
+	 double a=0;
+	 double sum=3000000;
+	 double r=0.03;
+	 double money=0;
+	 a=	Math.pow(1+r,30);
+	 money=sum/a;
+	 System.out.println("开始存款为:"+money);
+	  } 
+	 } 
+
+
+
